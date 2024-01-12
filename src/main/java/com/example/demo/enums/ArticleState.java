61d2f4bbe8064f105daa371da33c8ef85198cbc4
liaotingwei <ryougishiki23333@gmail.com>
2024-01-12 18:33:32 +0800

    12

---
+++ src/main/java/com/example/demo/enums/ArticleState.java
@@ -0,0 +1,26 @@
+package com.example.demo.type;
+
+public enum ArticleState {
+    DRAFT(0),
+    PUBLISHED(1),
+    DELETED(2);
+
+    private final int state;
+
+    ArticleState(int state) {
+        this.state = state;
+    }
+
+    public int getState() {
+        return this.state;
+    }
+
+    public static ArticleState fromState(int state) {
+        for (ArticleState articleState : ArticleState.values()) {
+            if (articleState.getState() == state) {
+                return articleState;
+            }
+        }
+        throw new IllegalArgumentException("Invalid state: " + state);
+    }
+}
